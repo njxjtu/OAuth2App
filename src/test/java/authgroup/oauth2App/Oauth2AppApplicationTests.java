3935255f3b7db15d94aa2ea42caee307f4e15ea7
Nan Jiang <njxjtu@gmail.com>
2025-10-26 15:11:15 -0700

    Initial checkin

---
+++ src/test/java/authgroup/oauth2App/Oauth2AppApplicationTests.java
@@ -0,0 +1,13 @@
+package authgroup.oauth2App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
